6df1e26de5aeec20c7e2b9c25cad4a618bc62ee4
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2021-03-08 05:59:31 -0500

    Initial socket.io capability (players connect over localhost).

---
+++ src/server.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "http";
+import { Server as socketio} from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new socketio(httpServer, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET"]
+  }
+});
+
+httpServer.listen(3000);
+console.log('Listening on port 3000');
+
+io.on('connection', socket => {
+  console.log('Player connected!', socket.id);
+});
