2c531de049aa0fd1942c74e83a6240043f00ce07
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2021-03-14 07:52:07 -0400

    Added logic for determining the winner of the tic tac toe game.

--- src/games/tic-tac-toe.mjs
+++ src/games/tic-tac-toe.mjs
@@ -1,6 +1,9 @@
 import { corePropsAtBoot, coreTransitionLogic, corePlayerLogic } from '../game.mjs';
 
+// NOTE: This is pulled out in case we want to refer to it later.
+// E.g., an updated reset() method.
 const initialState = {
+  // A 3x3 grid with cell capable of holding a mark from a player.
   grid: Array.from({length:9}, (v,i) => {
     return {
       mark: null,
@@ -8,6 +11,7 @@ const initialState = {
       col: i % 3
     }
   }),
+  // Only two possile marks in this two player game.
   marks: ['x','o']
 }
 
@@ -16,22 +20,26 @@ const gameState = g => {
   return {
     ...g,
 
-    state: initialState,
+    state: initialState, // NOTE: Should we shallow copy?
 
   }
 }
 
-// If there are any top-level properties we want to define or overide
+// If there are any game properties we want to define or overide
 const gameProps = g => {
   return {
     ...g,
 
+    theWinner: null,
+
+    // NOTE: We don't have to just ammend the players property.
+    // We could also, for instance, 
     decorators: {
       addPlayer(gameToDecorate) {
         return {
           players: gameToDecorate.players.map((p,i) => ({
             ...p,
-            mark: gameToDecorate.state.marks[i]
+            mark: gameToDecorate.state.marks[i] // TODO: Throw error when i > 1
           }))
         };
       }
@@ -47,17 +55,56 @@ const gameActions = g => {
 
     makeMark(playerId, row, col) {
       const playerMark = this.players.filter(p => p.id === playerId).map(p => p.mark)[0];
+
+      const updatedGrid = this.state.grid.map(g => {
+        if (g.row === row && g.col === col) {
+          g.mark = playerMark;
+        }
+        return g;
+      });
+
       return {
         ...this,
         state: {
           ...this.state,
-          grid: this.state.grid.map((g,i) => {
-            if (g.row === row && g.col === col) {
-              g.mark = playerMark;
-            }
-            return g;
-          })
+          grid: updatedGrid
         }
+      }
+    },
+
+    findTheWinner() {
+      const possibleThreeInARows = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+      ];
+
+      const winningCells = possibleThreeInARows.reduce((acc,p3r) => {
+        const possibleCells = this.state.grid.reduce((pc,cc,ci) => {
+          if (ci === p3r[0] || ci === p3r[1] || ci === p3r[2]) {
+            return pc.length === 0 ?
+              [cc] :
+              pc[pc.length-1].mark === cc.mark  && cc.mark != null ?
+              pc.concat(cc) :
+              [cc];
+          } else {
+            return pc;
+          }
+        },[]);
+        return possibleCells.length === 3 ?
+          acc.concat(possibleCells) :
+          acc;
+      },[]);
+
+      const winningMark = winningCells.length === 3 ?
+        winningCells[0].mark :
+        null;
+
+      const winningPlayer = this.players.filter(p => p.mark === winningMark).map(p => p.id)[0];
+
+      return {
+        ...this,
+        theWinner: winningPlayer
       }
     }
   }
--- test/core/tic-tac-toe.test.mjs
+++ test/core/tic-tac-toe.test.mjs
@@ -16,6 +16,8 @@ describe('Tic Tac Toe', function() {
     });
   });
 
+  // TODO: Should assign a player a mark when they join the game.
+
   it('Should update state when marks are added to the grid', function() {
     let game = {...game0}; // shallow copy
     game = game.addPlayer('First','1').addPlayer('Second','2');
@@ -29,8 +31,27 @@ describe('Tic Tac Toe', function() {
       ],
       marks: ['x','o']
     });
-
   });
+
+  it('Should find the winner if there is a three-in-a-row', function() {
+    let game = {...game0}; // shallow copy
+    game = game.addPlayer('First','1').addPlayer('Second','2');
+    game = game.makeMark('1', 0, 0).makeMark('2', 1, 1).
+      makeMark('1', 0, 1).makeMark('2', 1, 0).
+      makeMark('1', 0, 2).findTheWinner();
+
+    game.should.have.property('state').deep.equal({
+      grid: [
+        { mark: 'x', row: 0, col: 0}, { mark: 'x', row: 0, col: 1}, { mark: 'x', row: 0, col: 2},
+        { mark: 'o', row: 1, col: 0}, { mark: 'o', row: 1, col: 1}, { mark: null, row: 1, col: 2},
+        { mark: null, row: 2, col: 0}, { mark: null, row: 2, col: 1}, { mark: null, row: 2, col: 2}
+      ],
+      marks: ['x','o']
+    });
+
+    game.should.have.property('theWinner').equal('1');
+    
+  });  
 
   
 
