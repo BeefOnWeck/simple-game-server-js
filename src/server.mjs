f65775f55db97f5a700048316b1389b53a2148be
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2021-03-22 20:32:31 -0400

    Improved logic with how players are added. Now players send their name with acknowledgement, so the server can send back a helpful error status message if a name is sent outside of the setup phase.

--- src/server.mjs
+++ src/server.mjs
@@ -73,51 +73,71 @@ io.on('connection', socket => { // TODO: Reject if we already have all the playe
   // NOTE: We may want to generalize this in case setup is more than just 
   //       picking a name. What if players need to take turns placing pieces 
   //       in the game?
-  socket.once('send-user-name', username => {
+  socket.on('send-user-name', (username, callback) => {
     // TODO: Throw error on null or empty name?
     socket.username = username === null ? '' : username;
     console.log('username', username);
 
-    // Add them to the game
-    // TODO: Players can't join until setup phase begins
-    // TODO: Test on more than 2 players
-    game = game.addPlayer(username, socket.id);
+    // TODO: Upgrade to Node 14 so we can use nullish coalescing
+    if (game.phase && game.phase === 'boot') {
+      callback({status: 'Setup has not started yet'});
+    } else if (game.phase && game.phase == 'setup') {
+      // Add them to the game
+      // TODO: Test on more than 2 players
+      game = game.addPlayer(username, socket.id);
+      callback({status: 'You have been added'});
+    
+      if (game.phase === 'play') {
+        // TODO: Also announce start of game?
+        io.emit('game-state', // TODO: Rename message to 'game-status'
+          game.getGameStatus()
+        );
+        // Tell player 1 it's their turn
+        io.to(game.firstPlayerId).emit('start-your-turn', {});
 
-    // Check the criteria for starting play
-    if (game.phase === 'play') { // We have all the players, start the game
-      
-      // TODO: Once we enter the play phase, players can't join anymore
-      io.emit('game-state', // TODO: Rename message to 'game-status'
-        game.getGameStatus()
-      );
+        // This is a point where things could break (if player 1 doesn't get the message, we're stuck)
+        // How to ensure we get confirmation?
+        // Send with acknowledgement: https://socket.io/docs/v3/emitting-events/
+      }
 
-      // Tell player 1 it's their turn
-      io.to(game.firstPlayerId).emit('start-your-turn', {}); // TODO: Consider sending list of actions here? Also send state
-      
-      // This is a point where things could break (if player 1 doesn't get the message, we're stuck)
-      // How to ensure we get confirmation?
-      // Send with acknowledgement: https://socket.io/docs/v3/emitting-events/
+    } else if (game.phase && game.phase == 'play') {
+      callback({status: 'Game has already started'});
+    } else {
+      callback({status: 'Game has not booted yet'});
     }
+
   });
 
   // When a player sends their action(s)
   socket.on('player-actions', actions => {
-    if (game.phase === 'play' && socket.id === game.activePlayerId) {
-      game = game.processActions(actions);
-    } // TODO: Else throw error?
-  });
+    if (game.phase === 'play') {
+      if (socket.id === game.activePlayerId) {
+        game = game.processActions(actions);
+      } else {
+        socket.emit('It is not your turn', {});
+      }
+    } else {
+      socket.emit('Play has not started yet', {});
+    }
+  }); 
 
   // When a player ends their turn
   socket.on('end-my-turn', () => {
-    if (game.phase === 'play' && socket.id === game.activePlayerId) {
-      console.log('Player is done:', game.activePlayerId);
-
-      // But first let's just go to the next player
-      game = game.nextPlayer();
-      io.emit('game-state', // TODO: Rename message to 'game-status'
-        game.getGameStatus()
-      );
-      io.to(game.activePlayerId).emit('start-your-turn', {});
+    if (game.phase === 'play') {
+      if (socket.id === game.activePlayerId) {
+        console.log('Player is done:', game.activePlayerId);
+        // But first let's just go to the next player
+        game = game.nextPlayer();
+        io.emit('game-state', // TODO: Rename message to 'game-status'
+          game.getGameStatus()
+        );
+        io.to(game.activePlayerId).emit('start-your-turn', {});
+      } else {
+        socket.emit('It is not your turn', {});
+      }
+    } else {
+      socket.emit('Play has not started yet', {});
     }
   });
+
 });
