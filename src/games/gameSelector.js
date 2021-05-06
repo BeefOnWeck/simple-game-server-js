a1b22dbfba0724102043b1e1a6e49b70828c5137
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2021-05-06 06:00:00 -0400

    Removing now unneeded dependency on lodash.clonedeep.

--- src/games/gameSelector.js
+++ src/games/gameSelector.js
@@ -1,5 +1,3 @@
-import cloneDeep from 'lodash.clonedeep';
-
 // import all games here
 import { game0 as t3 } from './tic-tac-toe/tic-tac-toe.js';
 import { game0 as blackjack } from './blackjack/blackjack.js';
@@ -11,8 +9,6 @@ const games = [
 
 export const selectGame = function(name, config = {}) {
   // TODO: Check for valid key and throw error if necessary
-  // TODO: If I implement a proper reset() function then the clone won't be necessary.
-  // let newGame = cloneDeep(games.filter(g => g.meta.name === name)[0]);
   let newGame = games.filter(g => g.meta.name === name)[0].reset();
   // Apply any configuration
   newGame = newGame.configureGame(config);
